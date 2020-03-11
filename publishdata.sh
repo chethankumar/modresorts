02a9c1ee4433344eadfd1569ebc605f35ccc8e7c
CharanKumar <charankumar.palla@gmail.com>
2020-03-11 10:10:19 +0530

    data

--- publishdata.sh
+++ publishdata.sh
@@ -8,9 +8,7 @@ node > out_${jsonFile} <<EOF
 var data1 = require('./${jsonFile}');
 //Manipulate data
 var data = {};
-# if(!data1[0].label){
   data1[0].label = data1[0].filename;
-# }
 data = data1[0];
 data.author = 'admin';
 data.author_display = 'admin';
